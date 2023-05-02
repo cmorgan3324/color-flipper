7268636e9189b3ff14d5a7d25ca80578af1a0736
Cory Morgan <132379707+cmorgan3324@users.noreply.github.com>
2023-05-02 11:53:46 -0400

    Initial upload

---
+++ CFscript.js
@@ -0,0 +1,17 @@
+const colors = ["green", "red", "rgba(133, 122, 200)", "#f15025"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+// eventlistener function targets body, body is property on document object
+btn.addEventListener("click", function () {
+  // get random number between 0-3; indexes in colors array
+  const randomNumber = getRandomNumber();
+  console.log(randomNumber);
+
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length); // colors.length = 4; Math.floor rounds DOWN to nearest integer
+}
